e8a8ca330ccc05b350276fe5b3fc699f38a8c7ea
Colin James Wood <christhefennec@gmail.com>
2026-05-28 16:05:18 +0100

    create templates for pico_display platforms to make it easier for cmake to import

---
+++ templates/optionals_display/pico_display.h
@@ -0,0 +1,3 @@
+// These files are not to be included by the library compile script.
+// Instead, copy and paste it into a new folder in the optionals folder, and include that with a CMakeLists.txt.
+
