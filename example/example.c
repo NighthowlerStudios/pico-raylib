54f13cfae17f626518439d330a470353959f69e1
Colin James Wood <christhefennec@gmail.com>
2026-06-04 13:50:11 +0100

    fix example intellisense and move all shapes to within lcd

--- example/example.c
+++ example/example.c
@@ -14,8 +14,6 @@
 ********************************************************************************************/
 
 #include "raylib.h"
-#include "pico/stdlib.h"
-#include "pico_display.h"
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -49,33 +47,34 @@ int main(void)
 
             ClearBackground(RAYWHITE);
 
-            DrawText("some basic shapes available on raylib", 20, 20, 20, DARKGRAY);
+            DrawText("some basic shapes available on raylib", 20, 20, 10, DARKGRAY);
+            DrawFPS(20, 40);
 
             // Circle shapes and lines
-            DrawCircle(screenWidth/5, 120, 35, DARKBLUE);
-            DrawCircleGradient((Vector2){ screenWidth/5.0f, 220.0f }, 60, GREEN, SKYBLUE);
-            DrawCircleLines(screenWidth/5, 340, 80, DARKBLUE);
-            DrawEllipse(screenWidth/5, 120, 25, 20, YELLOW);
-            DrawEllipseLines(screenWidth/5, 120, 30, 25, YELLOW);
+            DrawCircle(screenWidth/5, 60, 17, DARKBLUE);
+            DrawCircleGradient((Vector2){ screenWidth/5.0f, 110.0f }, 30, GREEN, SKYBLUE);
+            DrawCircleLines(screenWidth/5, 170, 40, DARKBLUE);
+            DrawEllipse(screenWidth/5, 60, 12, 10, YELLOW);
+            DrawEllipseLines(screenWidth/5, 60, 15, 12, YELLOW);
 
             // Rectangle shapes and lines
-            DrawRectangle(screenWidth/4*2 - 60, 100, 120, 60, RED);
-            DrawRectangleGradientH(screenWidth/4*2 - 90, 170, 180, 130, MAROON, GOLD);
-            DrawRectangleLines(screenWidth/4*2 - 40, 320, 80, 60, ORANGE);  // NOTE: Uses QUADS internally, not lines
+            DrawRectangle(screenWidth/4*2 - 60, 50, 60, 30, RED);
+            DrawRectangleGradientH(screenWidth/4*2 - 90, 85, 90, 65, MAROON, GOLD);
+            DrawRectangleLines(screenWidth/4*2 - 40, 160, 40, 30, ORANGE);  // NOTE: Uses QUADS internally, not lines
 
             // Triangle shapes and lines
-            DrawTriangle((Vector2){ screenWidth/4.0f *3.0f, 80.0f },
-                         (Vector2){ screenWidth/4.0f *3.0f - 60.0f, 150.0f },
-                         (Vector2){ screenWidth/4.0f *3.0f + 60.0f, 150.0f }, VIOLET);
+            DrawTriangle((Vector2){ screenWidth/4.0f *3.0f, 40.0f },
+                         (Vector2){ screenWidth/4.0f *3.0f - 30.0f, 75.0f },
+                         (Vector2){ screenWidth/4.0f *3.0f + 30.0f, 75.0f }, VIOLET);
 
-            DrawTriangleLines((Vector2){ screenWidth/4.0f*3.0f, 160.0f },
-                              (Vector2){ screenWidth/4.0f*3.0f - 20.0f, 230.0f },
-                              (Vector2){ screenWidth/4.0f*3.0f + 20.0f, 230.0f }, DARKBLUE);
+            DrawTriangleLines((Vector2){ screenWidth/4.0f*3.0f, 80.0f },
+                              (Vector2){ screenWidth/4.0f*3.0f - 10.0f, 115.0f },
+                              (Vector2){ screenWidth/4.0f*3.0f + 10.0f, 115.0f }, DARKBLUE);
 
             // Polygon shapes and lines
-            DrawPoly((Vector2){ screenWidth/4.0f*3, 330 }, 6, 80, rotation, BROWN);
-            DrawPolyLines((Vector2){ screenWidth/4.0f*3, 330 }, 6, 90, rotation, BROWN);
-            DrawPolyLinesEx((Vector2){ screenWidth/4.0f*3, 330 }, 6, 85, rotation, 6, BEIGE);
+            DrawPoly((Vector2){ screenWidth/4.0f*3, 175 }, 6, 40, rotation, BROWN);
+            DrawPolyLines((Vector2){ screenWidth/4.0f*3, 175 }, 6, 45, rotation, BROWN);
+            DrawPolyLinesEx((Vector2){ screenWidth/4.0f*3, 175 }, 6, 42, rotation, 6, BEIGE);
 
             // NOTE: We draw all LINES based shapes together to optimize internal drawing,
             // this way, all LINES are rendered in a single draw pass
